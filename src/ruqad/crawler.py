df4fab50e5ca9fbf8883be2c0fa5929f5fd9c193
Alexander Schlemmer <a.schlemmer@indiscale.com>
2024-11-14 11:08:17 +0100

    FIX: correct file and path names for file entities

--- src/ruqad/crawler.py
+++ src/ruqad/crawler.py
@@ -6,8 +6,7 @@
 import os
 from importlib import resources
 from os import walk
-from os.path import join, sep
-from pathlib import Path
+from os.path import join
 
 import linkahead as db
 from caoscrawler.crawl import crawler_main
@@ -23,25 +22,29 @@ def trigger_crawler(target_dir: str):
     caosdb-crawler -i crawler/identifiables.yaml -s update crawler/cfood.yaml <target_dir>
     ```
     """
-    target_dir = os.path.relpath(target_dir)
 
     # insert all .zip and .eln files, if they do not yet exist
     for fp, ds, fs in walk(target_dir):
         for fn in fs:
             if fn.endswith(".eln") or fn.endswith(".zip"):
-                file_ent = db.File(file=join(fp, fn),
-                                   path=join(*(fp.split(sep)[1:]), fn))
-                print(f"retrieve {join(fp,fn)}")
+
+                file_path = join(target_dir, fp, fn)
+                file_entity = join(fp[len(target_dir):], fn)
+                file_ent = db.File(file=file_path,
+                                   path=file_entity)
+                print(f"retrieve {join(fp, fn)}")
                 file_ent.retrieve()
                 if file_ent.id is None:
-                    print(f"insert {join(fp,fn)}")
+                    print(f"insert {join(fp, fn)}")
                     file_ent.insert()
                 else:
-                    print(f"update {join(fp,fn)}")
+                    print(f"update {join(fp, fn)}")
                     file_ent.update()
 
+    breakpoint()
     print("crawl", target_dir)
     crawler_main(crawled_directory_path=target_dir,
                  cfood_file_name=ruqad_crawler_settings.joinpath('cfood.yaml'),
-                 identifiables_definition_file=ruqad_crawler_settings.joinpath('identifiables.yaml'),
+                 identifiables_definition_file=ruqad_crawler_settings.joinpath(
+                     'identifiables.yaml'),
                  remove_prefix="/"+os.path.basename(target_dir))
