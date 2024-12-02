a13bf412ee6f63728afaaf2a92f3a69589eb0c33
Alexander Schlemmer <a.schlemmer@indiscale.com>
2024-12-02 13:48:54 +0100

    Optional "s" in url for kadi repo

--- end-to-end-tests/test_crawler.py
+++ end-to-end-tests/test_crawler.py
@@ -30,5 +30,6 @@ def test_crawl():
     crawl a directory as it would be created by export from kadi and running a data quality check
     """
     print(os.listdir(DATADIR))
-    trigger_crawler(os.fspath(DATADIR))
-    assert False
+    # TODO: return value for successful crawler
+    retval = trigger_crawler(os.fspath(DATADIR))
+    assert retval
