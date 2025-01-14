e85b17b9457cd453f1a5e0ee061cba4d2604dd32
Timm Fitschen <t.fitschen@indiscale.com>
2025-01-14 14:24:20 +0100

    FIX: update example config files

--- secrets.example.sh
+++ secrets.example.sh
@@ -8,15 +8,17 @@
 
 ## Kadi
 # Host and token to retrieve data
-KADIHOST=https://demo-kadi4mat.iam.kit.edu
+KADIHOST=http://localhost/kadi
 KADITOKEN=pat_KADI123456789
 
 ## S3
 # Key ID and secret to access the S3 bucket defined in `qualitycheck_config.toml`.
-S3_ACCESS_KEY_ID=456S3S3S3654
-S3_SECRET_ACCESS_KEY=123S3S3S3987
+#S3_ACCESS_KEY_ID=456S3S3S3654
+#S3_SECRET_ACCESS_KEY=123S3S3S3987
 
 ## Gitlab
 # Tokens to trigger a pipeline run and to get pipeline status and result via the API.
-GITLAB_PIPELINE_TOKEN=glptt-123456789
-GITLAB_API_TOKEN=glpat-987654321
+#GITLAB_PIPELINE_TOKEN=glptt-123456789
+#GITLAB_API_TOKEN=glpat-987654321
+
+SKIP_QUALITY_CHECK=True
